dcadcce97cd6cdb214738251a7380782444a8670
Simon FREMAUX <simon.fremaux@proton.me>
2024-11-08 16:11:50 +0100

    first commit

---
+++ login.js
@@ -0,0 +1,43 @@
+// Insert your code here
+const backendURL = "http://localhost:3000";
+
+// Register button
+document.querySelector("#register").addEventListener("click", function () {
+    const user = {
+        name: document.querySelector("#registerName").value,
+        email: document.querySelector("#registerEmail").value,
+        password: document.querySelector("#registerPassword").value,
+    };
+
+    fetch(`${backendURL}/users/signup`, {
+        method: "POST",
+        header: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result) {
+            window.location.assign("index.html");
+        }
+    });
+});
+
+// Login button
+document.querySelector("#connection").addEventListener("click", function () {
+    const user = {
+        email: document.querySelector("#connectionEmail").value,
+        password: document.querySelector("#connectionPassword").value,
+    };
+
+    fetch(`${backendURL}/users/signin`, {
+        method: "POST",
+        header: { "Content-Type": "application/json" },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result) {
+            window.location.assign("index.html");
+        }
+    });
+});
